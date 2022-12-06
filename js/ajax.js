61cfa69e3487012a69628770379d8bc6b149f017
junyu33 <37526026+junyu33@users.noreply.github.com>
2022-12-06 16:25:13 +0800

    Update ajax.js

--- js/ajax.js
+++ js/ajax.js
@@ -1,4 +1,4 @@
 $("#btn").click(() => {
    var value = $("#input").val();
-   $.get(`https://level10.junyu33.me/?${value}`, (data) => {alert(data)});
+   $.get(`https://level10.nazo.junyu33.me/?${value}`, (data) => {alert(data)});
 })
